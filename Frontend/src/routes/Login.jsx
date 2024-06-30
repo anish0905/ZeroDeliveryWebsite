c9d6fde214c45189fa5343070fa8d239ee691cf3
RamnarayanMandal <ramnarayan847231@gmail.com>
2024-07-01 02:00:38 +0530

    login with otp

--- Frontend/src/routes/Login.jsx
+++ Frontend/src/routes/Login.jsx
@@ -1,11 +1,55 @@
-import React from "react";
-import { Button, Dialog, DialogFooter } from "@material-tailwind/react";
-import logo from "../image/Blinkit.webp"
+import React, { useState } from "react";
+import { Button, Dialog } from "@material-tailwind/react";
+import logo from "../image/Blinkit.webp";
+import axios from "axios";
+import { API_URI } from "../../src/Contants";
+import { OTPInput } from "../Component/OTPInput";
 
 export function Login() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [otpSent, setOtpSent] = useState(false);
+  const [otp, setOtp] = useState("");
 
   const handleOpen = () => setOpen(!open);
+
+  const handleMobileNumberChange = (e) => {
+    setMobileNumber(e.target.value);
+  };
+
+  const handleOtpChange = (e) => {
+    setOtp(e.target.value);
+  };
+
+  const requestOtp = async () => {
+    try {
+      const resp = await axios.post(`${API_URI}/user/request-otp`, {
+        mobileNumber: `+91${mobileNumber}`
+      });
+      if (resp.status === 200) {
+        setOtpSent(true);
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const verifyOtp = async () => {
+    try {
+      const resp = await axios.post(`${API_URI}/user/verify-otp`, {
+        mobileNumber: `+91${mobileNumber}`,
+        otp
+      });
+      console.log(resp.data);
+      localStorage.setItem('token', resp.data.token);
+
+      if (resp.status === 200) {
+        setOpen(false);
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   return (
     <>
@@ -16,36 +60,41 @@ export function Login() {
         Login
       </Button>
       <Dialog open={open} handler={handleOpen}>
-        <div className="flex justify-center items-center content-center ">
-         
-         <img src={logo} alt="" className="w-52 h-28" />
-       
+        <div className="flex justify-center items-center content-center">
+          <img src={logo} alt="Blinkit Logo" className="w-52 h-28" />
         </div>
 
-        <div className="flex justify-center flex-col content-center items-center font-bold ">
+        <div className="flex justify-center flex-col content-center items-center font-bold">
           <p className="text-black text-2xl mb-1">India's last minute app</p>
           <p className="text-base mb-1">Log in or Sign up</p>
-          <div className="w-72">
-            <div className="relative w-full min-w-[200px] h-10">
-              <input
-                className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline-none focus:outline-none disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
-                placeholder=" "
-              />
-              <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-                +91
-              </label>
-            </div>
-          </div>
+          {otpSent ? (
+            <OTPInput otp={otp} handleOtpChange={handleOtpChange} verifyOtp={verifyOtp} />
+          ) : (
+            <>
+              <div className="w-72">
+                <div className="relative w-full min-w-[200px] h-10">
+                  <input
+                    className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline-none focus:outline-none disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+                    placeholder=" "
+                    value={mobileNumber}
+                    onChange={handleMobileNumberChange}
+                  />
+                  <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
+                    +91
+                  </label>
+                </div>
+              </div>
+              <div className="flex justify-center items-center content-center my-2 w-11/12">
+                <Button onClick={requestOtp} className="w-6/12 bg-blue-gray-600">
+                  <span>Continue</span>
+                </Button>
+              </div>
+            </>
+          )}
         </div>
-
-       
-        <div className="flex justify-center items-center content-center my-2  w-full">
-        <Button   onClick={handleOpen} className="w-6/12 bg-blue-gray-600" >
-            <span>Continue</span>
-          </Button>
-        </div>
-        <p className="text-xs text-center mb-4 mt-2">By continuing, you agree to our Terms of service & Privacy policy</p>
-     
+        <p className="text-xs text-center mb-4 mt-2">
+          By continuing, you agree to our Terms of service & Privacy policy
+        </p>
       </Dialog>
     </>
   );
