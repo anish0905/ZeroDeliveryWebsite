efe721a7d715c6f386d3762475f82a949af43489
RamnarayanMandal <ramnarayan847231@gmail.com>
2024-07-03 02:02:56 +0530

    modify it

--- Frontend/src/Component/AddressForm.jsx
+++ Frontend/src/Component/AddressForm.jsx
@@ -8,9 +8,12 @@ const mapContainerStyle = {
   flex: 1,
   height: '400px',
 };
+const longitude = localStorage.getItem('longitude');
+
+const latitude = localStorage.getItem('latitude');
 const center = {
-  lat: 12.9716, // Example latitude for Bengaluru
-  lng: 77.5946, // Example longitude for Bengaluru
+  lat: longitude,
+  lng: latitude // Example longitude for Bengaluru
 };
 
 const AddressForm = () => {
@@ -35,8 +38,8 @@ const AddressForm = () => {
   if (!isLoaded) return 'Loading Maps';
 
   return (
-    <div className="p-1 max-w-4xl mx-auto flex ">
-      <div className="w-1/2 pr-2 mt-28">
+    <div className="p-1 max-w-4xl mx-auto lg:flex md:flex block gap-10 px-10  pt-28 ">
+      <div className="lg:w-1/2 md:w-1/2 w-full my-8  ">
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           zoom={15}
@@ -48,7 +51,7 @@ const AddressForm = () => {
           ))}
         </GoogleMap>
       </div>
-      <div className="w-1/2 pl-4 mt-28">
+      <div className="lg:w-1/2 md:w-1/2 w-full my-4 ">
         <form className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">Save address as *</label>
@@ -69,24 +72,24 @@ const AddressForm = () => {
           </div>
           <div className="space-y-1">
             <label className="block text-sm font-medium text-gray-700">Flat / House no / Building name *</label>
-            <input type="text" className="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" />
+            <input type="text" className="block w-full border-2 solid border-black rounded pl-5 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" />
           </div>
           <div className="space-y-1">
             <label className="block text-sm font-medium text-gray-700">Floor (optional)</label>
-            <input type="text" className="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" />
+            <input type="text" className="block w-full border-2 pl-5 solid border-black rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" />
           </div>
           <div className="space-y-1">
             <label className="block text-sm font-medium text-gray-700">Area / Sector / Locality *</label>
-            <input type="text" className="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" defaultValue=" Rajajinagar, Bengaluru" />
+            <input type="text" className="block w-full  rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm border-2 pl-5 solid border-black" defaultValue=" Rajajinagar, Bengaluru" />
           </div>
           <div className="space-y-1">
             <label className="block text-sm font-medium text-gray-700">Nearby landmark (optional)</label>
-            <input type="text" className="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" />
+            <input type="text" className="block w-full rounded pl-5 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm border-2 solid border-black" />
           </div>
           <div className="space-y-1">
             <label className="block text-sm font-medium text-gray-700">Enter your details for seamless delivery experience</label>
-            <input type="text" placeholder="Your name *" className="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm" />
-            <input type="text" placeholder="Your phone number (optional)" className="block w-full border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500  h-8 sm:text-sm mt-2" defaultValue="7260858715" />
+            <input type="text" placeholder="Your name *" className="block w-full  rounded pl-5 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 h-8 sm:text-sm border-2 solid border-black" />
+            <input type="number" placeholder="Your phone number (optional)" className="block w-full pl-5 rounded shadow-sm focus:ring-indigo-500 focus:border-indigo-500  h-8 sm:text-sm mt-2 border-2 solid border-black" defaultValue="7260858715" />
           </div>
           <button type="submit" className="flex items-center justify-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600">
             <FiEdit className="mr-2" /> Save
