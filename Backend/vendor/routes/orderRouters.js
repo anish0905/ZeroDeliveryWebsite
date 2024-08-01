6ed36c86230a4f22608868ae43928a8ce0740806
anish0905 <anishanish0102@gmail.com>
2024-08-01 23:44:12 +0530

    last month and lastseven days

--- Backend/vendor/routes/orderRouters.js
+++ Backend/vendor/routes/orderRouters.js
@@ -5,5 +5,7 @@ const order = require("../controller/orderController");
 router.get("/placeOrder/:vendorUserId", order.getOrdersByVendorUser);
 
 router.put("/changeOrderStatus", order.changeStatus);
+router.get("/products/lastThirtyDays/:vendorId", order.getProductsLastThirtyDays);
+router.get("/products/lastSevenDays/:vendorId", order.getProductsLastSevenDays);
 
 module.exports = router;
