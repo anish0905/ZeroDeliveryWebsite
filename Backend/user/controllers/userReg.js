1bc4412e7a18718ffcf6230b0b8623b3200c0274
Anish kumar <anishanish0102@gmail.com>
2024-10-02 11:43:38 +0530

    updated

--- Backend/user/controllers/userReg.js
+++ Backend/user/controllers/userReg.js
@@ -1,5 +1,5 @@
 const User = require('./userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const transporter = require('./mailer');
 const crypto = require('crypto');
