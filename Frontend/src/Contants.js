e317eaddd24bec1ffc8c83e04edc20b48a6bd8c1
Ramnarayan Mandal <154451608+RamnarayanMandal@users.noreply.github.com>
2024-10-20 21:39:23 +0530

    Update Contants.js

--- Frontend/src/Contants.js
+++ Frontend/src/Contants.js
@@ -4,4 +4,4 @@
 // export const API_URI = "https://blinkitserver-2.onrender.com";
 
 
-export const API_URI = "http://localhost:5001";
+export const API_URI = "https://zerodeliverywebsite.onrender.com";
